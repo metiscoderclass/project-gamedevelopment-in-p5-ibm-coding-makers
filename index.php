8908d3c380be16f44de456f20c27bc0fc5cc52f6
bilal ayachi <bilal.ayachi@hetmml.nl>
2017-11-09 18:37:19 +0000

    de inlog page mooier gemaakt

--- index.php
+++ index.php
@@ -1,8 +1,7 @@
 <?php
 
-$naam= $_POST['username'];
-$wachtwoord = $_POST['password'];
-$confirm_wachtwoord = $_POST['confirm-password'];
+$username = $_POST['username'];
+$password = $_POST['password'];
 
 $host = "localhost";
 $user = "ayachi2003";
@@ -12,20 +11,9 @@ $port = 3306;
 
 $connection = mysqli_connect($host, $user, $pass, $db, $port);
 
-$sql = "INSERT INTO users(username,wachtwoord) VALUES('$naam','$wachtwoord')";
+$sql = "INSERT INTO users('username','wachtwoord') VALUES($username,$password)";
 
-if (isset($_POST["register-submit"])) {
-  if ($wachtwoord != $confirm_wachtwoord) {
-      echo "<script>alert('fout Wachtwoord of gebruikersnaam')</script>";
-  }
-  elseif ($wachtwoord == "" or $naam == "") {
-    echo "<script>alert('vul je gegevens')</script>";
-  }
-   else {
-    mysqli_query($connection,$sql);
-  }
-
-}
+$result = mysqli_query($connection,$sql);
 
 
 mysqli_close($connection);
@@ -51,79 +39,79 @@ mysqli_close($connection);
     </head>
     
     
-    <body>
+    <body style="background-color: red">
+  
+ 
+  
         
-<div class="container">
-    	<div class="row">
-			<div class="col-md-6 col-md-offset-3">
-				<div class="panel panel-login">
-					<div class="panel-heading">
-						<div class="row">
-							<div class="col-xs-6">
-								<a href="#" class="active" id="login-form-link">Login</a>
-							</div>
-							<div class="col-xs-6">
-								<a href="#" id="register-form-link">Register</a>
-							</div>
-						</div>
-						<hr>
-					</div>
-					<div class="panel-body">
-						<div class="row">
-							<div class="col-lg-12">
-								<form id="login-form" action="server.php" method="post" role="form" style="display: block;">
-									<div class="form-group">
-										<input type="text" name="naam" id="username" tabindex="1" class="form-control" placeholder="Username" value="">
-									</div>
-									<div class="form-group">
-										<input type="password" name="wachtwoord" id="password" tabindex="2" class="form-control" placeholder="Password">
-									</div>
-									<div class="form-group text-center">
-										<input type="checkbox" tabindex="3" class="" name="remember" id="remember">
-										<label for="remember"> Remember Me</label>
-									</div>
-									<div class="form-group">
-										<div class="row">
-											<div class="col-sm-6 col-sm-offset-3">
-												<input type="submit" name="login-submit" id="login-submit" tabindex="4" class="form-control btn btn-login" value="Log In">
-											</div>
-										</div>
-									</div>
-									<div class="form-group">
-										<div class="row">
-											<div class="col-lg-12">
-												<div class="text-center">
-													<a href="#" tabindex="5" class="forgot-password">Forgot Password?</a>
-												</div>
-											</div>
-										</div>
-									</div>
-								</form>
-								<form id="register-form" method="post" role="form" style="display: none;">
-									<div class="form-group">
-										<input type="text" name="username" id="username" tabindex="1" class="form-control" placeholder="Username" value="">
-									</div>
-									<div class="form-group">
-										<input type="password" name="password" id="password" tabindex="2" class="form-control" placeholder="Password">
-									</div>
-									<div class="form-group">
-										<input type="password" name="confirm-password" id="confirm-password" tabindex="2" class="form-control" placeholder="Confirm Password">
-									</div>
-									<div class="form-group">
-										<div class="row">
-											<div class="col-sm-6 col-sm-offset-3">
-												<input type="submit" name="register-submit" id="register-submit" tabindex="4" class="form-control btn btn-register" value="Register Now">
-											</div>
-										</div>
-									</div>
-								</form>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
+ <div id="fullscreen_bg" class="fullscreen_bg"/>
+<div id="regContainer" class="container">
+      <div class="row">
+      <div class="col-md-6 col-md-offset-3">
+        <div class="panel panel-login">
+          <div class="panel-heading">
+            <div class="row">
+              <div class="col-xs-6">
+                <a href="#" class="active" id="login-form-link">Login</a>
+              </div>
+              <div class="col-xs-6">
+                <a href="#" id="register-form-link">Register</a>
+              </div>
+            </div>
+            <hr>
+          </div>
+          <div class="panel-body">
+            <div class="row">
+              <div class="col-lg-12">
+                <form id="login-form" action="server.php" method="post" role="form" style="display: block;">
+                  <div class="form-group">
+                    <label for="username">naam</label>
+                    <input type="text" name="naam" id="username" tabindex="1" class="form-control" placeholder="Username" value="">
+                  </div>
+                  <div class="form-group">
+                    <label for="password">wachtwoord</label>
+                    <input type="password" name="wachtwoord" id="password" tabindex="2" class="form-control" placeholder="Password">
+                  </div>
+                  <div class="form-group text-center">
+                    <input type="checkbox" tabindex="3" class="" name="remember" id="remember">
+                    <label for="remember"> Remember Me</label>
+                  </div>
+                  <div class="form-group">
+                    <div class="row">
+                      <div class="col-sm-6 col-sm-offset-3">
+                        <input type="submit" name="login-submit" id="login-submit" tabindex="4" class="form-control btn btn-login" value="Log In">
+                      </div>
+                    </div>
+                  </div>
+                </form>
+                <form id="register-form" method="post" role="form" style="display: none;">
+                  <div class="form-group">
+                    <label for="username">naam</label>
+                    <input type="text" name="username" id="username" tabindex="1" class="form-control" placeholder="naam" value="">
+                  </div>
+                  <div class="form-group">
+                    <label for="password">wachtwoord</label>
+                    <input type="password" name="password" id="password" tabindex="2" class="form-control" placeholder="wachtwoord">
+                  </div>
+                  <div class="form-group">
+                    <label for="confirm-password">Confirm password</label>
+                    <input type="password" name="confirm-password" id="confirm-password" tabindex="2" class="form-control" placeholder="Confirm wachtwoord">
+                  </div>
+                  <div class="form-group">
+                    <div class="row">
+                      <div class="col-sm-6 col-sm-offset-3">
+                        <input type="submit" name="register-submit" id="register-submit" tabindex="4" class="form-control btn btn-register" value="Register Now">
+                      </div>
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
     
 
         
