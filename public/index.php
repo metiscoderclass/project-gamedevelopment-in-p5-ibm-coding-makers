d8c736ce2d56757eeeef6cbf6fc7d4601be30db1
bilal ayachi <bilal.ayachi@hetmml.nl>
2017-11-09 18:11:49 +0000

    Home background toegevoegt

--- public/index.php
+++ public/index.php
@@ -2,6 +2,20 @@
 <html>
     
     
+    <head>
+        
+        <link rel="stylesheet" href="css/index.css" type="text/css" />
+        
+    </head>
+    
+    <body class="home">
+        
+        
+        
+    </body>
+    
+
+    
     
     
 </html>
